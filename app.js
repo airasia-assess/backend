1005f183e22cd023296ec521410125c06557dd77
Ashkan <ashkan65@hotmail.com>
2021-01-30 17:15:12 +0800

    change start status

--- app.js
+++ app.js
@@ -36,5 +36,4 @@ app.use("/", appRouter);
 app.use("/api/auth", authRouter);
 
 const port = process.env.port || 1026;
-const http = require("http").createServer(app);
-http.listen(port, () => console.log(`${enums.msgs.SERVER_CONNECT} ${port}`));
+app.listen(port, () => console.log(`${enums.msgs.SERVER_CONNECT} ${port}`));
